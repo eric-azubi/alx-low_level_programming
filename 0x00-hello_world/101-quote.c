e4cc4aebae06998897732a99210bee079f96be46
eric <ericamesibotchway@gmail.com>
2022-03-10 13:53:25 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - print to standard error
+ * Description: write function
+ * Return: return 1
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
+}
