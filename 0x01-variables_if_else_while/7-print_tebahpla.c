9f47c16672d3399b5423732f5fc9e5ad6153e1e3
eric <ericamesibotchway@gmail.com>
2022-03-11 15:57:59 -0800

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+i#include <stdio.h>
+
+/**
+ *main - Prints lowercase letters in reverse order using only putchar
+ *
+ *Description: Prints lowercase letters in reverse order using only putchar
+ *Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
