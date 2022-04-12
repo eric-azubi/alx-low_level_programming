568725b8a3f227434d988fa740b74c1a4d650913
Eric <64174003+eric-azubi@users.noreply.github.com>
2022-04-12 00:53:23 +0000

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Write a program that prints the name of the file it was compiled
- *   * from, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ * main - Write a program that prints the name of the file it was compiled
+ * from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-		printf("%s\n", __FILE__);
-			return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
